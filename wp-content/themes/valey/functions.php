93182b3813db94692cd7b64211c2525dcdfda406
Mishra <shashank@clothchronicles.com>
2018-04-24 20:08:59 +0530

    changes are there related to removing valey theme error

--- wp-content/themes/valey/functions.php
+++ wp-content/themes/valey/functions.php
@@ -5,7 +5,7 @@
  * @since voga
  **/
 $_template_path = get_template_directory();
-require_once $_template_path."/framework/abstract.php";
+//require_once $_template_path."/framework/abstract.php";
 $theme = new WdTheme(array(
 	'theme_name'	=>	"Voga",
 	'theme_slug'	=>	'voga'
