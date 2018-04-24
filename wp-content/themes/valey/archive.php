c460858a9cb4a88bc5ce61da5ae5c61e6c80d83e
Mishra <shashank@clothchronicles.com>
2018-04-24 20:13:01 +0530

    changes are there related to removing valey theme error

--- wp-content/themes/valey/archive.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * The template for displaying archive pages.
- *
- * @since   1.0.0
- * @package Valey
- */
-
-get_template_part( 'index' );
--- wp-content/themes/valey/search.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * The template for displaying search results pages.
- *
- * @since   1.0.0
- * @package Valey
- */
-
-get_template_part( 'index' );
